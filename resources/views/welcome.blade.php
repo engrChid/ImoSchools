c8960aa6a454b2b02f484f1fa8ecaea7162e8329
engrChidi <mrchidi@codinglab.ng>
2017-04-21 01:48:43 -0700

    configurations like Routes, Database, Controller for Schools.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Imo Schools
                 </div>
 
                 <div class="links">
